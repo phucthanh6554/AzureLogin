850c989dd48d84ba600f663412b18adcff158ca0
Phuc Nguyen <phuc.nguyen@siliconstack.com.au>
2022-10-24 20:21:10 +0700

    Azure login with .net

---
+++ BE/AzureLoginDemo/AzureLoginDemo/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace AzureLoginDemo.Controllers
+{
+    [Route("account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("LoginAzure")]
+        public async Task<IActionResult> LoginAzure([FromBody] LoginAzureRequest request)
+        {
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, request.AccessToken);
+
+            var getUserInfoResponse = await client.PostAsync("https://graph.microsoft.com/oidc/userinfo", null);
+
+            getUserInfoResponse.EnsureSuccessStatusCode();
+
+            // If response status code is not Ok => throw err or something
+
+            // If resposne is Ok => Generate JWT Token here
+
+            return Ok(new LoginAzureResponse
+            {
+                Token = "Sample Token"
+            });
+        }
+    }
+
+    public class LoginAzureRequest
+    {
+        public string AccessToken { get; set; }
+    }
+
+    public class LoginAzureResponse
+    {
+        public string Token { get; set; }
+    }
+}
---
+++ BE/AzureLoginDemo/AzureLoginDemo/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("my-cors", policy =>
+    {
+        policy.WithOrigins("http://localhost:4000");
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("my-cors");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
